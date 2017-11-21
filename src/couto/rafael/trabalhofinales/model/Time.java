7325a0c6edc1f5e299a05a4a1453954a026f928e
rafael couto <rcouto97@gmail.com>
2017-11-21 09:30:17 -0200

    Time+Observer

--- src/couto/rafael/trabalhofinales/model/Time.java
+++ src/couto/rafael/trabalhofinales/model/Time.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package couto.rafael.trabalhofinales.model;
 
-/**
- *
- * @author developer
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Time {
+    private static int currentTime;
+    private static int endOfTime;
+    private static Time instance;
+    private List<TimeObserver> observers;
     
+    public Time(int endOfTime){
+        Time.endOfTime = endOfTime;
+        currentTime = 0;
+        observers = new ArrayList<>();
+    }
+    
+    public static Time getInstance(){
+        if (instance == null)
+            instance = new Time(0);
+        
+        return instance;
+    }
+    
+    public static void init(int endOfTime){
+        Time.endOfTime = endOfTime;
+    }
+    
+    public void notifyObservers(){
+        observers.forEach((observer) -> {
+            observer.timeChanged(currentTime);
+        });
+    }
+    
+    public void run(){
+        while(currentTime < endOfTime){
+            currentTime++;
+            
+            notifyObservers();
+        }            
+    }
+    
+    public void addTimeObserver(TimeObserver observer){
+        observers.add(observer);
+    }
+    
+    public void removeTimeObserver(TimeObserver observer){
+        observers.remove(observer);
+    }
 }
--- src/couto/rafael/trabalhofinales/model/TimeObserver.java
+++ src/couto/rafael/trabalhofinales/model/TimeObserver.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package couto.rafael.trabalhofinales.model;
 
-/**
- *
- * @author developer
- */
 public interface TimeObserver {
-    
+    public void timeChanged(int newTime);
 }
