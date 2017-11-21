477eae4d3ee14a3fe702111068933cd11e777a29
rafael couto <rcouto97@gmail.com>
2017-11-21 10:58:36 -0200

    Main e Observer iniciados

--- src/br/edu/ifrs/canoas/trabalhofinales/model/ConcreteTimeObserver.java
+++ src/br/edu/ifrs/canoas/trabalhofinales/model/ConcreteTimeObserver.java
@@ -1,9 +1,17 @@
 package br.edu.ifrs.canoas.trabalhofinales.model;
 
-public class ConcreteTimeObserver implements TimeObserver{
+import java.util.Random;
 
+public class ConcreteTimeObserver implements TimeObserver{
+    public static final double STOP_CHANCE = 0.1;
+    public static final double VEHICLE_CHANCE = 0.5;
+    public static final int STOP_TIME = 20;
+    public static final int GO_TIME = 30;
+    
     @Override
     public void timeChanged(int newTime) {
-        System.out.println("Tempo atualizado: "+newTime);
+        Random random = new Random();
+        
+        
     }
 }
--- src/br/edu/ifrs/canoas/trabalhofinales/view/TrabalhoFinalES.java
+++ src/br/edu/ifrs/canoas/trabalhofinales/view/TrabalhoFinalES.java
@@ -1,8 +1,16 @@
 package br.edu.ifrs.canoas.trabalhofinales.view;
 
-public class TrabalhoFinalES {
+import br.edu.ifrs.canoas.trabalhofinales.model.ConcreteTimeObserver;
+import br.edu.ifrs.canoas.trabalhofinales.model.Time;
 
+public class TrabalhoFinalES {
+    public static final int DURATION = 600;
+    
+    static Time time = Time.getInstance();
+    int counter;
+    
     public static void main(String[] args) {
-        
+        time.addTimeObserver(new ConcreteTimeObserver());
+        Time.init(DURATION);
     }
 }
